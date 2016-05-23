3e1d2e601ef6c371ade2d685c9cc31154aed7235
Kai <tingnapianhai@gmail.com>
2016-05-23 13:55:22 +0200

    Complete the menu-badge stuff
    - if the badge-number is 0, set badge-number to 0, background color is transparent;
    - if the badge-number is 1-99, set badge-number to 1-99, background color is red;
    - if the badge-number is >99, set badge-number to 99+, background color is red;

--- app/src/main/java/com/tingapianhai/android/testbed/actionbarmenuitem/MyActionBarActivity.java
+++ app/src/main/java/com/tingapianhai/android/testbed/actionbarmenuitem/MyActionBarActivity.java
@@ -1,5 +1,7 @@
 package com.tingapianhai.android.testbed.actionbarmenuitem;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,11 +10,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tingapianhai.android.testbed.R;
 
+import org.w3c.dom.Text;
+
 public class MyActionBarActivity extends AppCompatActivity {
+
+    Context con;//not a good way to do this
+    private ImageView badgeImageView;
+    private TextView badgeTextView;
+    private int badgeNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,8 @@ public class MyActionBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_action_bar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        con = this;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -34,13 +47,51 @@ public class MyActionBarActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.my_actionbar_menu_itembadge, menu);
+        View menu_badge = menu.findItem(R.id.myactionbar_menu_badge_item).getActionView();
+        badgeTextView = (TextView) menu_badge.findViewById(R.id.actionbar_menu_badge_textview);
+        badgeImageView = (ImageView) menu_badge.findViewById(R.id.action_menu_badge_image);
+        badgeImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ++ badgeNumber;
+                setBadgeNumber(badgeNumber);
+            }
+        });
+        //badgeImageView.setOnClickListener(new MyBadgeItemClickListener());
         return true;
+    }
+
+    /**
+     * set up a.badge-item background color; b.badge-text-number
+     * @param badgeNumber
+     */
+    private void setBadgeNumber(int badgeNumber){
+        if(badgeNumber>99){
+            badgeTextView.setText("99+");//if the badge-number > 99, then use 99+ instead of 104,229,375 etc..
+            badgeTextView.setBackgroundColor(Color.argb(255,255,0,0));//set badge-text view background to red-color
+        } else if(badgeNumber>0){
+            badgeTextView.setText(""+badgeNumber);
+            badgeTextView.setBackgroundColor(Color.argb(255,255,0,0));//set badge-text view background to red-color
+        } else{
+            badgeTextView.setBackgroundColor(Color.argb(0,0,0,0));//set badge-text view background to transparent
+        }
+    }
+
+    /**
+     * can be removed !!!
+     * Badge view OnClickListener
+     */
+    private class MyBadgeItemClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(con,"badge clicked",Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.securemessages_engagements_item) {
+        if (id == R.id.myactionbar_menu_badge_item) {
             return true;
         }
 
